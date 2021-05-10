e3a16a05b5914f2330abebb8ba523ef3faf766db
Matheuspff <Mortytheus05@gmail.com>
2021-05-10 01:36:34 -0400

    First commit

---
+++ interface/name.php
@@ -0,0 +1,3 @@
+___| Ferramenta By __|
+|  Morty__|
+|__________|
---
+++ src/help.js
@@ -0,0 +1,205 @@
+const //database
+	return `
+┏🔥࿗ 𝙼𝚘𝚛𝚝𝚢-𝚋𝚘𝚝 𝚞𝚕𝚝𝚒𝚖𝚊𝚝𝚎 ࿗🔥┓
+║                                                           
+║ _*🕐 𝐃𝐚𝐭𝐚 𝐞 𝐡𝐨𝐫𝐚: ${date} 🕐*_
+║ _*🙂 𝐔𝐬𝐮𝐚́𝐫𝐢𝐨: ${user} 🙂*_
+║ _*🌎 𝐖𝐚𝐦𝐞: ${wame} 🌎*_   
+║                                     
+╠══✪〘 INFO 〙✪══
+║
+╠➥ 𝐌𝐎𝐑𝐓𝐘 𝐁𝐎𝐓
+╠➥ wa.me/556598049058
+╠➥ 𝐃𝐎𝐍𝐎: 𝐌𝐎𝐑𝐓𝐘 
+╠➥ 𝐒𝐓𝐀𝐓𝐔𝐒: 𝐎𝐍
+║
+┗═════════════════════┛
+
+  ★彡༻⚡𝙽𝚘𝚟𝚒𝚍𝚊𝚍𝚎𝚜⚡༺彡★
+࿇ ═══━━━━━✥◈✥━━━━━═══ ࿇
+
+➻❥ *${prefix}qrcode [premium]*
+➻❥ *${prefix}chentai [premium]*
+➻❥ *${prefix}gcpf [premium]*
+➻❥ *${prefix}gay [@]*
+➻❥ *${prefix}gbin [premium]*
+➻❥ *${prefix}pack [premium]*
+➻❥ *${prefix}destrava [premium]*
+➻❥ *${prefix}gpessoa [premium]*
+➻❥ *${prefix}wame*
+➻❥ *${prefix}spamcall*
+➻❥ *${prefix}play (nome da msc)*
+
+࿇ ════━━━━━✥◈✥━━━━━════ ࿇
+     ıllıllı༻✨𝙼𝚎𝚗𝚞 𝙼𝚘𝚛𝚝𝚢✨༺ıllıllı
+࿇ ════━━━━━✥◈✥━━━━━════ ࿇
+
+➻❥ *${prefix}figu*
+➻❥ *${prefix}toimg*
+➻❥ *${prefix}meme*
+➻❥ *${prefix}memeindo*
+➻❥ *${prefix}tts*
+➻❥ *${prefix}lolih [on]*
+➻❥ *${prefix}nsfwloli [off]*
+➻❥ *${prefix}url2img*
+➻❥ *${prefix}leens [na legenda]*
+➻❥ *${prefix}wait [na legenda]*
+➻❥ *${prefix}setprefix*
+
+࿇ ════━━━━━✥◈✥━━━━━════ ࿇
+      ☆.｡.*༻📌𝙾𝚞𝚝𝚛𝚘𝚜📌༺*.｡.☆
+࿇ ════━━━━━✥◈✥━━━━━════ ࿇
+
+➻❥ *${prefix}linkgp*
+➻❥ *${prefix}simih [1/0]*
+➻❥ *${prefix}marcar*
+➻❥ *${prefix}add [@]*
+➻❥ *${prefix}banir [@]*
+➻❥ *${prefix}promover [@]*
+➻❥ *${prefix}rebaixar*
+➻❥ *${prefix}admins*
+➻❥ *${prefix}marcar2*
+➻❥ *${prefix}bc [texto]* (ele faz uma ™)
+➻❥ *${prefix}marcar3*
+➻❥ *${prefix}bloqueados*
+➻❥ *${prefix}bloquear [@]*
+➻❥ *${prefix}desbloquear [@]*
+➻❥ *${prefix}limpar*
+➻❥ *${prefix}bc [ *texto* ]*
+➻❥ *${prefix}bemvindo [1/0]*
+➻❥ *${prefix}clonar [@]*
+➻❥ *${prefix}help1*
+➻❥*${prefix}dono*
+➻❥ *${prefix}owner*
+➻❥ *${prefix}tts [texto]*
+➻❥ *${prefix}setnome*
+➻❥ *${prefix}setfoto*
+➻❥ *${prefix}grupoinfo*
+➻❥ *${prefix}ytmp4*
+➻❥ *${prefix}bomdia*
+➻❥ *${prefix}boanoite*
+➻❥ *${prefix}marcar*
+➻❥ *${prefix}marcar2*
+➻❥ *${prefix}marcar3*
+
+࿇ ════━━━━━✥◈✥━━━━━════ ࿇
+        .｡○༻📷𝙸𝚖𝚊𝚐𝚎𝚗𝚜📷༺○｡.
+࿇ ════━━━━━✥◈✥━━━━━════ ࿇
+
+➻❥ *${prefix}loli* [off]
+➻❥ *${prefix}loli1*
+➻❥ *${prefix}hentai*
+➻❥ *${prefix}dono*
+➻❥ *${prefix}porno*
+➻❥ *${prefix}boanoite*
+➻❥ *${prefix}bomdia*
+➻❥ *${prefix}boatarde*
+➻❥ *${prefix}mia*
+➻❥ *${prefix}mia1*
+➻❥ *${prefix}mia2*
+➻❥ *${prefix}belle*
+➻❥ *${prefix}belle1*
+➻❥ *${prefix}belle2*
+➻❥ *${prefix}belle3*
+➻❥ *${prefix}akeno*
+➻❥ *${prefix}meme*   
+➻❥ *${prefix}lofi*
+➻❥ *${prefix}malkova*
+➻❥ *${prefix}canal*
+➻❥ *${prefix}nsfwloli1*
+➻❥ *${prefix}reislin*
+
+
+ ࿇ ════━━━━━✥◈✥━━━━━════ ࿇
+•.¸♡༻🤖𝙸𝚗𝚝𝚎𝚕𝚒𝚐𝚎𝚗𝚌𝚒𝚊 𝚋𝚘𝚝🤖༺♡¸.•
+ ࿇ ════━━━━━✥◈✥━━━━━════ ࿇
+
+➻❥ *${prefix}simih 1 (para ativar)*
+➻❥ *${prefix}simih 0 (para desativar)*
+➻❥ *{prefix}simi (sua mensagem)*
+
+࿇ ════━━━━━✥◈✥━━━━━════ ࿇
+    ✦✧✧༻💸𝙿𝚛𝚎𝚖𝚒𝚞𝚖💸༺✧✧✦
+࿇ ════━━━━━✥◈✥━━━━━════ ࿇
+
+➻❥ *${prefix}dado*
+➻❥ *${prefix}cekvip*
+➻❥ *${prefix}premiumlist*
+➻❥ *${prefix}delete*
+➻❥ *${prefix}modapk*
+➻❥ *${prefix}indo10*
+➻❥ *${prefix}daftarvip [para virar Premium]*
+➻❥ *${prefix}qrcode*
+➻❥ *${prefix}chentai*
+➻❥ *${prefix}gcpf*
+➻❥ *${prefix}gbin*
+➻❥ *${prefix}pack*
+➻❥ *${prefix}destrava*
+➻❥ *${prefix}gpessoa*
+
+࿇ ════━━━━━✥◈✥━━━━━════ ࿇
+╚»★«╝༻👥𝙶𝚛𝚞𝚙𝚘𝚜👥༺╚»★«╝
+࿇ ════━━━━━✥◈✥━━━━━════ ࿇
+
+➻❥ *${prefix}banir*
+➻❥ *${prefix}leveling [on/off]*
+➻❥ *${prefix}level*
+➻❥ *${prefix}add*
+➻❥ *${prefix}promover*
+➻❥ *${prefix}setfoto [na legenda]*
+➻❥ *${prefix}setname [texto]*
+➻❥ *${prefix}rebaixar*
+➻❥ *${prefix}admins*
+➻❥ *${prefix}marcar*
+➻❥ *${prefix}marcar2*
+➻❥ *${prefix}marcar3*
+➻❥ *${prefix}bemvindo [1/0]*
+➻❥ *${prefix}grupoinfo*
+➻❥ *${prefix}bomdia*
+➻❥ *${prefix}boatarde*
+➻❥ *${prefix}boanoite*
+➻❥ *${prefix}setdesc*
+➻❥ *${prefix}bug [sua mensagem]*
+
+
+      ࿇ ════━━━━━✥◈✥━━━━━════ ࿇
+☆.｡.:*༻🍪𝙴𝚜𝚙𝚎𝚌𝚒𝚏𝚒𝚌𝚘 𝚍𝚘 𝙱𝚘𝚝🍪༺*:.｡.☆
+      ࿇ ════━━━━━✥◈✥━━━━━════ ࿇
+
+➻❥ *${prefix}bug [sua mensagem]*
+➻❥ *${prefix}clonar [@]*
+➻❥ *${prefix}dono*
+➻❥ *${prefix}ping [ver velocidade do bot]*
+➻❥ *${prefix}termux*
+➻❥ *${prefix}gay [@]*
+➻❥ *${prefix}wame*
+➻❥ *${prefix}map (nome)*
+➻❥ *${prefix}setppbot (marque uma img)*
+➻❥ *${prefix}pinterest (nome)*
+➻❥ *${prefix}desligar (so para o dono)*
+➻❥ *${prefix}timer*
+
+
+࿇ ════━━━━━✥◈✥━━━━━════ ࿇
+ ✧･ﾟ:༻🥶𝙼𝚊𝚒𝚜 𝚊𝚕𝚐𝚞𝚗𝚜🥶༺:･ﾟ✧
+࿇ ════━━━━━✥◈✥━━━━━════ ࿇
+
+➻❥ *${prefix}neko*
+➻❥ *${prefix}ttp [texto]*
+➻❥ *${prefix}testime*
+➻❥ *${prefix}tomp3*
+➻❥ *${prefix}animehug*
+➻❥ *${prefix}shoturl*
+➻❥ *${prefix}pokemon*
+
+        ࿇ ════━━━━━✥◈✥━━━━━════ ࿇
+:..｡○༻📃𝙲𝚘𝚗𝚝𝚊𝚐𝚎𝚖 𝚍𝚘𝚜 𝚌𝚘𝚖𝚊𝚗𝚍𝚘𝚜📃༺○｡..:
+        ࿇ ════━━━━━✥◈✥━━━━━════ ࿇
+
+➻❥ *comandos no total: 128*
+
+➻❥〘 𝐌𝐎𝐑𝐓𝐘 𝐁𝐎𝐓〙`
+}
+
+//Online.database
+
